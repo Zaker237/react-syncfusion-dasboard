ac674a451fab8e8087eef28583041cebf1aaf66f
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-05-25 23:15:49 +0200

    Kanban App

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -9,7 +9,7 @@ import { Header } from '../components';
 const Calendar = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2
-    md:p-10 bg-white rounded-3xl'>
+      md:p-10 bg-white rounded-3xl'>
       <Header
         title='Calendar'
         category='App'
@@ -27,4 +27,4 @@ const Calendar = () => {
   )
 }
 
-export default Calendar
+export default Calendar;
