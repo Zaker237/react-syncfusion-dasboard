f91a6f41976a3eeed226596337b8bf0fa29749cc
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-05-26 03:25:07 +0200

    Area Chart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -13,8 +13,8 @@ const Area = () => {
     <div className='m-4 md:m-10 mt-24 p-10 bg-white
     dark:bg-secondary-dark-bg rounded-3xl'>
       <Header
-        title='Inflation Rate'
-        category='Chart'
+        title='Inflation Rate in Pourcentage'
+        category='Area Chart'
       />
       <div className='w-full'>
         <ChartComponent
